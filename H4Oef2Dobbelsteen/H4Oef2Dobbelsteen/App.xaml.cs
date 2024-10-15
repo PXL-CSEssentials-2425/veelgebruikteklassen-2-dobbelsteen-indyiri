49ace2bac3449ac7dba46d1a2892a5f2623eb104
Indy Iri <indy.iri@student.pxl.be>
2024-10-15 22:45:02 +0200

    WPF layout gemaakt

---
+++ H4Oef2Dobbelsteen/H4Oef2Dobbelsteen/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace H4Oef2Dobbelsteen
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
